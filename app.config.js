233650eaa4629019bcbe7e54dd27da52353e9209
Batara Hotdo <batarah07d0@gmail.com>
2025-06-10 22:40:45 +0700

    Delete Notification Sound

--- app.config.js
+++ app.config.js
@@ -10,68 +10,54 @@ module.exports = {
     splash: {
       image: "./assets/BudgetForSplashandAdaptive.png",
       resizeMode: "contain",
-      backgroundColor: "#ffffff"
+      backgroundColor: "#ffffff",
     },
     ios: {
-      supportsTablet: true
+      supportsTablet: true,
     },
     android: {
       adaptiveIcon: {
         foregroundImage: "./assets/BudgetForSplashandAdaptive.png",
-        backgroundColor: "#ffffff"
+        backgroundColor: "#ffffff",
       },
-      edgeToEdgeEnabled: true
+      package: "com.batarahotdo.budgetwise",
+      edgeToEdgeEnabled: true,
     },
     web: {
-      favicon: "./assets/favicon.png"
+      favicon: "./assets/favicon.png",
     },
     plugins: [
-      [
-        "expo-location",
-        {}
-      ],
+      ["expo-location", {}],
 
-      [
-        "expo-media-library",
-        {}
-      ],
+      ["expo-media-library", {}],
       [
         "expo-image-picker",
         {
-          "photosPermission": "Aplikasi memerlukan akses ke galeri foto Anda untuk memilih gambar.",
-          "cameraPermission": "Aplikasi memerlukan akses ke kamera Anda untuk mengambil foto."
-        }
+          photosPermission:
+            "Aplikasi memerlukan akses ke galeri foto Anda untuk memilih gambar.",
+          cameraPermission:
+            "Aplikasi memerlukan akses ke kamera Anda untuk mengambil foto.",
+        },
       ],
-      [
-        "expo-file-system",
-        {}
-      ],
-      [
-        "expo-sensors",
-        {}
-      ],
+      ["expo-file-system", {}],
+      ["expo-sensors", {}],
       [
         "expo-notifications",
         {
-          "icon": "./assets/BudgetWiseLogo.png",
-          "color": "#3B82F6",
-          "sounds": [
-            "./assets/notification-sound.wav"
-          ]
-        }
+          icon: "./assets/BudgetWiseLogo.png",
+          color: "#3B82F6",
+          // sounds: ["./assets/notification-sound.wav"],
+        },
       ],
-      [
-        "expo-local-authentication",
-        {}
-      ],
-      "expo-sqlite"
+      ["expo-local-authentication", {}],
+      "expo-sqlite",
     ],
     // Tambahkan konfigurasi untuk menangani polyfills
     extra: {
       eas: {
-        projectId: "your-project-id"
+        projectId: "your-project-id",
       },
-      enablePolyfills: true
-    }
-  }
+      enablePolyfills: true,
+    },
+  },
 };
